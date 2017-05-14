509dd81fe1a97728633be8eaa8dc7909ff269724
stephan-boyer <stephan.boyer@airbnb.com>
2017-05-14 04:02:25 -0700

    Add config for Travis CI

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@
 
 main.pdf: main.tex
 	mkdir -p "build"
-	pdflatex -output-directory "build" main.tex
+	pdflatex -interaction=nonstopmode -output-directory "build" main.tex
 	while ( \
 		grep -qi \
 			'^LaTeX Warning: Label(s) may have changed' \
 			"build/main.log" \
 	) do \
-		pdflatex -output-directory "build" main.tex; \
+		pdflatex -interaction=nonstopmode -output-directory "build" main.tex; \
 	done
 	mv build/main.pdf .
 
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+# This script builds the PDF and uploads it to S3.
+# It also installs the necessary dependencies.
+
+# Usage:
+#   TRAVIS_BRANCH=master \
+#   AWS_DEFAULT_REGION=us-east-1 \
+#   AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE \
+#   AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY \
+#   ./deploy.sh
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y python-pip texlive-full
+sudo pip install awscli
+make
+if [ "$TRAVIS_BRANCH" = 'master' ]; then
+  aws s3 cp --acl public-read main.pdf "s3://stephan-misc/paper/latest.pdf"
+else
+  aws s3 cp --acl public-read main.pdf "s3://stephan-misc/paper/branch-$TRAVIS_BRANCH.pdf"
+fi
