08329b024d3dada9df7b2e2152bb29dcaa1ec5ca
Stephan Boyer <stephan@stephanboyer.com>
2017-05-26 22:16:46 -0700

    Add helpful notation in the Coq script

--- formalization/definitions.v
+++ formalization/definitions.v
@@ -18,6 +18,8 @@ Inductive type : Type :=
 | tunit : type
 | tarrow : type -> type -> type.
 
+Notation "t1 '→' t2" := (tarrow t1 t2) (at level 38).
+
 (* Terms *)
 
 Inductive term : Type :=
@@ -25,6 +27,9 @@ Inductive term : Type :=
 | evar : id -> term
 | eabs : id -> type -> term -> term
 | eapp : term -> term -> term.
+
+Notation "'@' s" := (evar (makeId s)) (at level 10).
+Notation "'λ' s '∈' t '.' e" := (eabs (makeId s) t e) (at level 39).
 
 (* Contexts *)
 
@@ -41,30 +46,36 @@ Definition lookupVar (c : context) e :=
   | _ => None
   end.
 
+Notation "'Ø'" := emptyContext.
+Notation "c ',' s '∈' t" := (extendContext c (makeId s) t) (at level 39).
+Notation "c '[' e ']'" := (lookupVar c e) (at level 40).
+
 (*  Typing rules *)
 
-Reserved Notation "c '|-' t ':::' T" (at level 40).
+Reserved Notation "c '⊢' e '∈' t" (at level 40).
 
 Inductive hasType : context -> term -> type -> Prop :=
-| aunit : forall c, c |- eunit ::: tunit
-| avar : forall c e t, lookupVar c e = Some t -> c |- e ::: t
+| aunit : forall c, c ⊢ eunit ∈ tunit
+| avar : forall c e t, lookupVar c e = Some t -> c ⊢ e ∈ t
 | aabs : forall c i t1 t2 e,
-         (extendContext c i t1 |- e ::: t2) ->
-         (c |- eabs i t1 e ::: tarrow t1 t2)
+         (extendContext c i t1 ⊢ e ∈ t2) ->
+         (c ⊢ eabs i t1 e ∈ tarrow t1 t2)
 | aapp : forall c e1 e2 t1 t2,
-         c |- e1 ::: t1 ->
-         c |- e2 ::: tarrow t1 t2 ->
-         c |- eapp e2 e1 ::: t2
-where "c '|-' t ':::' T" := (hasType c t T).
+         c ⊢ e1 ∈ t1 ->
+         c ⊢ e2 ∈ tarrow t1 t2 ->
+         c ⊢ eapp e2 e1 ∈ t2
+where "c '⊢' e '∈' t" := (hasType c e t).
 
 (* An example proof *)
 
 Definition v_unit := eunit.
 
-Theorem v_tunit : emptyContext |- v_unit ::: tunit.
+Theorem v_tunit : emptyContext ⊢ v_unit ∈ tunit.
 Proof. apply aunit. Qed.
 
 Definition v_id := eabs (makeId "x") tunit (evar (makeId "x")).
 
-Theorem v_tid : emptyContext |- v_id ::: tarrow tunit tunit.
+Print v_id.
+
+Theorem v_tid : emptyContext ⊢ v_id ∈ tarrow tunit tunit.
 Proof. apply aabs. apply avar. simpl. reflexivity. Qed.
