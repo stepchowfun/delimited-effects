ec65c84d31a2c698fa21eae2a66ff19c31e2fdfe
Stephan Boyer <stephan@stephanboyer.com>
2017-06-25 17:32:05 -0700

    Set up Docker build infrastructure

--- deploy.sh
+++ deploy.sh
@@ -6,30 +6,32 @@ set -eu -o pipefail
 
 # Usage:
 #   TRAVIS_BRANCH=master \
+#   TRAVIS_PULL_REQUEST=false \
 #   AWS_DEFAULT_REGION=us-east-1 \
 #   AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE \
 #   AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY \
 #   ./deploy.sh
 
-# Install LaTeX and the AWS CLI
-DEBIAN_FRONTEND=noninteractive sudo apt-get -y update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y python-pip texlive-full
-sudo pip install awscli
+# Build the PDF, check the Coq scripts, and lint the source.
+make docker-build
 
-# Install Coq
-curl -o /tmp/coq-8.6.tar.gz https://s3.amazonaws.com/stephan-misc/paper/coq-8.6.tar.gz
-tar -xzf /tmp/coq-8.6.tar.gz -C /tmp
-cd /tmp/coq-8.6 && sudo make install && cd -
-rm -rf /tmp/coq-8.6
-
-# Build the PDF, check the Coq scripts, and lint the source
-make
-
-# Upload the PDF to S3
+# Upload the PDF to S3.
 if [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
   if [ "$TRAVIS_BRANCH" = 'master' ]; then
-    aws s3 cp --acl public-read main.pdf 's3://stephan-misc/paper/latest.pdf'
+    docker run \
+      -e "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" \
+      -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+      -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
+      -it stephanmisc/delimited-effects:build aws s3 cp --acl public-read \
+      /root/delimited-effects/main.pdf \
+      s3://stephan-misc/paper/latest.pdf
   else
-    aws s3 cp --acl public-read main.pdf "s3://stephan-misc/paper/branch-$TRAVIS_BRANCH.pdf"
+    docker run \
+      -e "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" \
+      -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+      -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
+      -it stephanmisc/delimited-effects:build aws s3 cp --acl public-read \
+      /root/delimited-effects/main.pdf \
+      "s3://stephan-misc/paper/branch-$TRAVIS_BRANCH.pdf"
   fi
 fi
