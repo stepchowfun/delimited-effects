c97602bff92c024c9bfabf261435c510b7587484
Esther Wang <esther.wang@airbnb.com>
2017-06-27 01:45:12 -0700

    Update syntax in coq

--- formalization/syntax.v
+++ formalization/syntax.v
@@ -6,65 +6,69 @@ Require Import Coq.Strings.String.
 Inductive id (T : Type) : Type :=
 | makeId : string -> id T.
 
-Inductive tid : Type := . (* Type id *)
+Inductive sid : Type := . (* Type id *)
 Inductive eid : Type := . (* Term id *)
 Inductive rid : Type := . (* Effect row id *)
 Inductive xid : Type := . (* Effect id *)
 
-Definition typeId := id tid.
+Definition schemeId := id sid.
 Definition termId := id eid.
-Definition effectRowId := id rid.
-Definition effectId := id xid.
-
-
-(* Types *)
-
-Inductive type : Type :=
-| tvar : typeId -> type
-| tunit : type
-| tarrow : type -> type -> type
-| ttforall : typeId -> type -> type
-| txforall : effectRowId -> type -> type.
 
 (* Terms *)
 
 Inductive term : Type :=
-| eunit : term
 | evar : termId -> term
-| eabs : termId -> type -> term -> term
-| eapp : term -> term -> term
-| etabs : typeId -> term -> term
-| etapp : term -> type -> term
-| exabs : effectRowId -> term -> term
-| exapp : term -> effects -> term
-| eeffect : effectId -> list (typeId) -> termId -> type -> term -> term
-| eprovide : effectId -> list type -> effects -> termId -> term -> term -> term
+| eabs : termId -> scheme -> term -> term
+| eappbv : term -> term -> term
+| eappbn : term -> term -> term
+| esabs : schemeId -> term -> term
+| esapp : term -> scheme -> term
+| eeffect : schemeId -> kind -> term -> term
+| eprovide : scheme -> termId -> term -> term -> term
+
+(* Schemes *)
+
+with scheme : Type :=
+| stwithx : type -> effects -> scheme
+| rrow : effects -> scheme
+| svar : schemeId -> scheme
+| sabs : schemeId -> kind -> scheme
+| sapp : scheme -> scheme -> scheme
+
+(* Types *)
+
+with type : Type :=
+| tarrow : type -> type -> type
+| ttforall : schemeId -> type -> type
 
 (* Effects *)
 
 with effects : Type :=
-| xvar : effectRowId -> effects
-| xempty : effects
-| xsingleton : effectId -> list type -> effects
-| xunion : effects -> effects.
+| rempty : effects
+| rsingleton : scheme -> effects
+| runion : effects -> effects -> effects
+
+(* Kinds *)
+
+with kind : Type :=
+| kkind : kind
+| krow : kind
+| keffect : schemeId -> termId -> scheme -> kind
+| karrow : schemeId -> kind -> kind -> kind.
 
 (* Type contexts *)
 
 Inductive context : Type :=
 | cempty : context
-| cextend : context -> termId -> type -> context.
-
-(* Effect context *)
-
-Inductive xContext : Type :=
-| dempty : xContext
-| dextend : xContext -> effectId -> list (typeId) -> termId -> type -> xContext.
+| ceextend : context -> termId -> scheme -> context
+| csextend : context -> schemeId -> kind -> context.
 
 (* Notation "'@'" := makeId (at level 10). *)
 Notation "t1 '→' t2" := (tarrow t1 t2) (at level 38).
 Notation "'λ' i '∈' t '⇒' e" := (eabs i t e) (at level 39).
 Notation "'Ø'" := cempty.
-Notation "c ',' i '∈' t" := (cextend c i t) (at level 39).
+Notation "c ',t' i '∈' s" := (ceextend c i s) (at level 39).
+Notation "c ',s' i '∈' k" := (csextend c i k) (at level 39).
 
 Definition eqId {X : Set} (i1 : id X) (i2 : id X) : bool :=
   match i1 with
@@ -73,11 +77,22 @@ Definition eqId {X : Set} (i1 : id X) (i2 : id X) : bool :=
     end
   end.
 
-Fixpoint lookupVar (c1 : context) e :=
+Fixpoint lookupEVar (c1 : context) e :=
   match e with
   | evar i1 => match c1 with
                | cempty => None
-               | cextend c2 i2 t => if eqId i1 i2 then Some t else lookupVar c2 e
+               | ceextend c2 i2 s => if eqId i1 i2 then Some s else lookupEVar c2 e
+               | csextend c2 i2 k => lookupEVar c2 e
+               end
+  | _ => None
+  end.
+
+Fixpoint lookupSVar (c1 : context) e :=
+  match e with
+  | svar i1 => match c1 with
+               | cempty => None
+               | ceextend c2 i2 s => lookupSVar c2 e
+               | csextend c2 i2 k => if eqId i1 i2 then Some k else lookupSVar c2 e
                end
   | _ => None
   end.
