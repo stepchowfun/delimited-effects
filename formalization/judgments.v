ea628417f4c1d0279174f8e8bafa78d25a2d86ea
Stephan Boyer <stephan@stephanboyer.com>
2017-06-27 21:14:57 -0700

    Add skeletons of the judgments

---
+++ formalization/judgments.v
@@ -0,0 +1,62 @@
+Require Import syntax.
+
+(* Effect row subsumption *)
+
+Inductive subsumes : row -> row -> Prop :=
+| rsRefl :
+    forall r,
+    subsumes r r
+(* TODO: Fill in the other rules here. *)
+
+(* Subtyping *)
+
+with isSubtypeOf : scheme -> scheme -> Prop :=
+| stRefl :
+    forall s,
+    isSubtypeOf s s
+(* TODO: Fill in the other rules here. *)
+
+(* Operation type well-formedness *)
+
+with opTypeWellFormed : scheme -> schemeId -> Prop :=
+| wfArrow :
+    forall s1 s2 a r,
+    opTypeWellFormed s2 a ->
+    opTypeWellFormed (stwithx (tarrow s1 s2) r) a
+(* TODO: Fill in the other rules here. *)
+
+(* Typing rules *)
+
+with hasType : context -> term -> scheme -> Prop :=
+| htVar :
+    forall c e s,
+    lookupEVar c e = Some s ->
+    hasType c e s
+(* TODO: Fill in the other rules here. *)
+
+(* Kinding rules *)
+
+with hasKind : context -> scheme -> kind -> Prop :=
+| hkArrow :
+    forall c s1 s2 r,
+    hasKind c s1 ktype ->
+    hasKind c s2 ktype ->
+    hasKind c (srow r) krow ->
+    hasKind c (stwithx (tarrow s1 s2) r) ktype
+(* TODO: Fill in the other rules here. *)
+
+(* Scheme equivalence *)
+
+with schemeEq : scheme -> scheme -> Prop :=
+| seRefl :
+    forall a b,
+    schemeEq a b
+(* TODO: Fill in the other rules here. *)
+
+(* Kind equivalence *)
+
+with kindEq : kind -> kind -> Prop :=
+| keRefl :
+    forall a b,
+    kindEq a b.
+(* TODO: Fill in the other rules here. *)
