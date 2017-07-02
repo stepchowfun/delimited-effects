b91792375fb24a9c65ac5dbc7fb464a69b83620b
Esther Wang <esther.wang@airbnb.com>
2017-07-02 15:58:29 -0700

    Rename context type extension constructor

--- formalization/syntax.v
+++ formalization/syntax.v
@@ -58,14 +58,14 @@ with kind : Type :=
 Inductive context : Type :=
 | cempty : context
 | ceextend : context -> termId -> type -> context
-| csextend : context -> typeId -> kind -> context.
+| ctextend : context -> typeId -> kind -> context.
 
 (* Notation "'@'" := makeId (at level 10). *)
 Notation "t1 '→' t2" := (ptarrow t1 t2) (at level 38).
 Notation "'λ' i '∈' t '⇒' e" := (eabs i t e) (at level 39).
 Notation "'Ø'" := cempty.
 Notation "c ',e' i '∈' t" := (ceextend c i t) (at level 39).
-Notation "c ',s' i '∈' k" := (csextend c i k) (at level 39).
+Notation "c ',s' i '∈' k" := (ctextend c i k) (at level 39).
 
 Definition eqId {X : Set} (i1 : id X) (i2 : id X) : bool :=
   match i1 with
@@ -81,7 +81,7 @@ Fixpoint lookupEVar (c1 : context) e :=
                | ceextend c2 i2 t => if eqId i1 i2
                                      then Some t
                                      else lookupEVar c2 e
-               | csextend c2 i2 k => lookupEVar c2 e
+               | ctextend c2 i2 k => lookupEVar c2 e
                end
   | _ => None
   end.
@@ -91,7 +91,7 @@ Fixpoint lookupTVar (c1 : context) e :=
   | tvar i1 => match c1 with
                | cempty => None
                | ceextend c2 i2 t => lookupTVar c2 e
-               | csextend c2 i2 k => if eqId i1 i2
+               | ctextend c2 i2 k => if eqId i1 i2
                                      then Some k
                                      else lookupTVar c2 e
                end
