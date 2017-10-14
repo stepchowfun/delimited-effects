e2446ff73e26b710fcaf4f9aa4bfd9208fd2717e
Esther Wang <esther.wang47@gmail.com>
2017-10-13 22:47:16 -0700

    Simplify the Arbitrary Row instance

--- implementation/src/Lib.hs
+++ implementation/src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib (Row(..), rowEquiv) where
 
-import Test.QuickCheck (Arbitrary, Gen, arbitrary, choose, shrink)
+import Test.QuickCheck (Arbitrary, Gen, arbitrary, choose, oneof, shrink)
 
 data Row a b = RVar b
              | REmpty
@@ -10,24 +10,13 @@ data Row a b = RVar b
   deriving (Eq, Show)
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Row a b) where
-  arbitrary = do
-    n <- choose (1, 5) :: Gen Int
-    case n of
-      1 -> do
-        v <- arbitrary
-        return $ RVar v
-      2 -> return REmpty
-      3 -> do
-        e <- arbitrary
-        return $ RSingleton e
-      4 -> do
-        x <- arbitrary
-        y <- arbitrary
-        return $ RUnion x y
-      5 -> do
-        x <- arbitrary
-        y <- arbitrary
-        return $ RDifference x y
+  arbitrary = oneof
+    [ RVar <$> arbitrary
+    , pure REmpty
+    , RSingleton <$> arbitrary
+    , RUnion <$> arbitrary <*> arbitrary
+    , RDifference <$> arbitrary <*> arbitrary
+    ]
   shrink (RVar x) = []
   shrink REmpty = []
   shrink (RSingleton x) = []
