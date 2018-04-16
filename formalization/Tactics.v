99ee2d8fd42bba551292e171e9425dd766cd41a6
Stephan Boyer <stephan@stephanboyer.com>
2018-04-16 03:30:36 -0700

    Add a formalization of System F

--- formalization/Tactics.v
+++ formalization/Tactics.v
@@ -8,42 +8,126 @@
 
 Require Import Omega.
 
-(* This is like the `inversion` tactic, but leaves less junk around. *)
-Ltac invert H := inversion H; clear H; repeat (
-  match goal with
-  | [ H : ?x = ?y |- _ ] => (is_var y; subst x) || (is_var x; subst y)
-  | [ H : ?x = ?x |- _ ] => clear H
-  end
-).
-
 (*
-  This tactic tries a variety of approaches to solve a goal. It uses the
-  resolve, rewrite, and unfold hints from the "core" database.
+  This tactic does a variety of simplifications on the goal and hypotheses.
+  It's used by the `magic` tactics below. If you just want to clean up the goal
+  and context for easier reading, use the `clean` tactic below.
 *)
 
-Ltac magic := try abstract (
-  cbn;
-  intros;
-  f_equal;
-  idtac + autounfold with core in *;
-  idtac + autorewrite with core in *;
-  omega + congruence + dintuition
-).
+Ltac simplify tactic :=
+  repeat (
+    intros;
+    cbn in *;
+    subst;
+    try (autorewrite with core in *);
+    try (
+      match goal with
+      | [ H : ex _ |- _ ] => destruct H
+      | [ H : _ /\ _ |- _ ] => destruct H
+      | [ H : _ <-> _ |- _ ] => destruct H
+      | [ H : ?T = ?T |- _ ] => clear H
+      | [ H1 : ?T -> _ |- _ ] =>
+        match type of T with
+        | context[Prop] =>
+          let H2 := fresh "H"
+          in assert (H2 : T); [
+            solve [ tactic ] |
+            specialize (H1 H2); clear H2
+          ]
+        end
+      end
+    )
+  ).
 
 (*
-  This tactic is useful if you have a hypothesis H : P -> Q and you want to
-  use Q. You can just write `feed H`. A new proof obligation for P will be
-  generated, and then the hypothesis will be specialized to H : Q.
+  The `magic` tactic tries a variety of approaches to solve a goal. The
+  `eMagic` tactic does everything `magic` does but uses `eauto` instead of
+  `auto`.
 *)
 
-Ltac feed H1 := let H2 := fresh "H" in
-  match type of H1 with
-  | ?T -> _ => assert (H2 : T); [ | specialize (H1 H2); clear H2 ]
-  end.
+Ltac magicWith tactic :=
+  try solve [tactic];
+  try solve [
+    simplify tactic;
+    try solve [dintuition (simplify tactic; tactic)];
+    try solve [progress f_equal; magicWith tactic];
+    try solve [congruence];
+    try solve [omega];
+    try solve [
+      match goal with
+      | [ |- context[if ?X then _ else _] ] =>
+        destruct X; magicWith tactic; fail
+      | [ _ : context[if ?X then _ else _] |- _ ] =>
+        destruct X; magicWith tactic; fail
+      | [ H : _ |- _ ] => inversion H; fail
+      end
+    ]
+  ].
+
+Ltac magic := let autoStar := auto with * in magicWith autoStar.
+
+Ltac eMagic := let eautoStar := eauto with * in magicWith eautoStar.
+
+(* This tactic cleans up the goal and context for easier reading. *)
+
+Ltac clean :=
+  let rec reorderContext :=
+    try match goal with
+    | [ H : ?T |- _ ] =>
+      match type of T with
+      | context[Prop] => revert H; reorderContext; intro H
+      end
+    | [ H : context[Prop] |- _ ] => revert H; reorderContext; intro H
+    end
+  in simplify magic; reorderContext.
 
 (*
-  This tactic takes a given term and adds its type to the context as a new
+  This tactic takes a given term and adds it to the context as a new
   hypothesis.
 *)
 
 Ltac fact E := let H := fresh "H" in pose (H := E); clearbody H.
+
+(*
+  This tactic is useful if you have a hypothesis H : P -> Q and you want to
+  use Q. You can just write `feed H`. A new proof obligation for P may be
+  generated, and then the hypothesis will be specialized to H : Q.
+*)
+
+Ltac feed H1 :=
+  let H2 := fresh "H"
+  in
+    match type of H1 with
+    | ?T -> _ => assert (H2 : T); [ | specialize (H1 H2); clear H2 ]
+    end; magic.
+
+(* This notation performs `generalize dependent` on multiple terms at once. *)
+
+Tactic Notation "gen" constr(e1) :=
+  generalize dependent e1.
+
+Tactic Notation "gen" constr(e1) constr(e2) :=
+  generalize dependent e2;
+  generalize dependent e1.
+
+Tactic Notation "gen" constr(e1) constr(e2) constr(e3) :=
+  generalize dependent e3;
+  generalize dependent e2;
+  generalize dependent e1.
+
+Tactic Notation "gen" constr(e1) constr(e2) constr(e3) constr(e4) :=
+  generalize dependent e4;
+  generalize dependent e3;
+  generalize dependent e2;
+  generalize dependent e1.
+
+Tactic Notation "gen" constr(e1) constr(e2) constr(e3) constr(e4) constr(e5) :=
+  generalize dependent e5;
+  generalize dependent e4;
+  generalize dependent e3;
+  generalize dependent e2;
+  generalize dependent e1.
+
+(* This is like the `inversion` tactic, but leaves less junk around. *)
+
+Ltac invert H := inversion H; try clear H; subst.
