dc91d4ff2250d48d52fdece041f1a5f13599989d
Stephan Boyer <stephan@stephanboyer.com>
2018-04-27 21:08:21 -0700

    Implement evaluation

---
+++ implementation/test/EvaluationSpec.hs
@@ -0,0 +1,9 @@
+module EvaluationSpec
+  ( evaluationSpec
+  ) where
+
+import Test.Hspec (Spec, describe, it, pending)
+
+-- The QuickCheck specs
+evaluationSpec :: Spec
+evaluationSpec = describe "eval" $ it "should be correct" pending
